dfbf19aa3fddf92dcda4b59b1fc8b9e0b60b4f41
alejandro7345 <140361461+alejandro7345@users.noreply.github.com>
2023-07-23 20:45:52 -0600

    Add files via upload

---
+++ numeros primos.cpp
@@ -0,0 +1,40 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	int a ;
+	int cont ,i, divs ;
+	
+	cont <=0;
+	
+	cout<<"  ingrece un numero entero positivo \n ";
+	cin>>a;
+	
+	
+	
+	for ( i=1 ; i<=a ; i++ ){
+	
+	 if (a%2<=0){
+	 	
+	 	cont=cont+1;
+	 	
+	 	
+	 }
+	 
+	}
+	
+	
+	
+	if(cont<=2){
+	 	cout<<"  el numero es primo "; 
+	 }else{
+	     cout<<"    el numero ingresado no es primo";
+	 }
+	 
+	 
+	 return 0;
+}
